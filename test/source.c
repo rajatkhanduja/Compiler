7aecc54ee84528a5d1745e8abc075ef78b35798a
Rajat Khanduja <rajatkhanduja13@gmail.com>
2012-01-27 17:27:54 +0530

    First commit of the branch SIMPylex_master

---
+++ test/source.c
@@ -0,0 +1,15 @@
+int aa = 00
+int bb = 10
+int ab
+aa = 10 + 00
+bb = 10 * 00
+ab = 10 - 00
+return ab aa
+if aa = 00
+break
+else
+continue
+void ba ( )
+{ 
+ba = 00
+}
---
+++ test/test.c
@@ -0,0 +1,16 @@
+/* Test int */
+int main (void)
+{
+        printf ("A test program, \" if else while\n");
+        float a = 0.0;
+        a = +5;
+
+        char c_int = 'a';
+	
+	a = -4 + 5.5;
+	a = -5.12 + (-12);
+        a++;
+        a = a + 1;
+                
+        return 0;
+}
