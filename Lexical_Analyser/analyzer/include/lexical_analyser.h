50a2e5dc219c3d31e3760a91d84acbc93bca1e16
Rajat Khanduja <rajatkhanduja13@gmail.com>
2012-03-07 02:44:27 +0530

    Added files to implement the lexical analyser.

---
+++ Lexical_Analyser/analyzer/include/lexical_analyser.h
@@ -0,0 +1,13 @@
+/* File that declares the Lexical Analyser class and its functions */
+#ifndef LEXICAL_ANALYSER_INCLUDED
+#define LEXICAL_ANALYSER_INCLUDED
+
+class LexicalAnalyser
+{
+	public:
+		LexicalAnalyser ();
+
+	private:
+};
+
+#endif	// End of file
